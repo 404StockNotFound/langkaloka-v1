ba1eff4671c7eb99944440baec9cbee4ade175db
setyawan-andy <andy.asbi@gmail.com>
2026-03-06 22:45:08 +0700

    initial setup db and project

---
+++ db/schema.ts
@@ -0,0 +1,25 @@
+import { boolean, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core"
+
+// Store table
+export const stores = pgTable("stores", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  name: text("name"),
+  description: text("description"),
+  isActive: boolean("is_active").notNull().default(true),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+})
+
+// Users table
+export const users = pgTable("users", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  storeId: uuid("store_id").references(() => stores.id, {
+    onDelete: "set null",
+  }),
+  email: text("email").notNull().unique(),
+  name: text("name"),
+  phone: text("phone").notNull().unique(),
+  address: text("address"),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+})
---
+++ drizzle/0000_brief_the_watchers.sql
@@ -0,0 +1,23 @@
+CREATE TABLE "stores" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" text,
+	"description" text,
+	"is_active" boolean DEFAULT true NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"store_id" uuid,
+	"email" text NOT NULL,
+	"name" text,
+	"phone" text NOT NULL,
+	"address" text,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email"),
+	CONSTRAINT "users_phone_unique" UNIQUE("phone")
+);
+--> statement-breakpoint
+ALTER TABLE "users" ADD CONSTRAINT "users_store_id_stores_id_fk" FOREIGN KEY ("store_id") REFERENCES "public"."stores"("id") ON DELETE set null ON UPDATE no action;
